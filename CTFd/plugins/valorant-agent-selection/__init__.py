d03361c8ce7f8647a45e869fd3ea6403ba56d2ed
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-05-29 12:56:26 +0800

    feat: check if agent was already chosen

--- CTFd/plugins/valorant-agent-selection/__init__.py
+++ CTFd/plugins/valorant-agent-selection/__init__.py
@@ -1,26 +1,27 @@
 from flask import render_template, Blueprint
 from flask import request, redirect, jsonify, session
 
-from CTFd.models import Users, Teams
+from CTFd.models import db, Users, Teams
 from CTFd.plugins import register_plugin_assets_directory
+from CTFd.utils.decorators import authed_only, require_team
+
+from .models import AgentChoice
+
+agent_selection = Blueprint('agent_selection', __name__, template_folder='templates')
 
 def load(app):
-	agent_selection = Blueprint('agent_selection', __name__, template_folder='templates')
+	app.db.create_all()
+
+	@authed_only
+	@require_team
 	@agent_selection.route('/agent', methods=['GET'])
 	def view_agent_page():
-		if not 'id' in session:
-			return redirect('/login')
-		user = Users.query.filter_by(id=session['id']).first()
-		if user is None:
-			return redirect('/login')
-
-		team_id = user.team_id
-		if team_id is None:
-			return redirect('/team?next=%2Fagent')
-		team = Teams.query.filter_by(id=user.team_id).first()
+		user = Users.query.filter(Users.id == session['id']).one()
+		team = db.session.query(Teams).filter_by(id=user.team_id).one()
 		if team.captain_id != session['id']:
 			return render_template('error.html', error="You are not the captain of this team")
-		# TODO: check if agent was already selected
+		if AgentChoice.query.filter_by(team_id=team.id).first() is not None:
+			return render_template('error.html', error="You have already selected agent")
 		return render_template('agent.html')
 
 	app.register_blueprint(agent_selection)
