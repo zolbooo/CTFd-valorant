709a7fc6d3a6c12970ba149edf8e4ca6686903a0
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-05-30 14:53:30 +0800

    feat: show agent artwork on card press

--- CTFd/plugins/valorant-agent-selection/assets/js/select-agent.js
+++ CTFd/plugins/valorant-agent-selection/assets/js/select-agent.js
@@ -1,6 +1,16 @@
-let selectedAgent = "test";
+let selectedAgent = null;
 function hydrate() {
-	// TODO
+	const artwork = document.getElementById('artwork');
+	const onClick = agent => () => {
+		selectedAgent = agent;
+		artwork.children[0].srcset = `/plugins/valorant-agent-selection/assets/agents/${agent}/artwork.webp`;
+		artwork.children[1].src = `/plugins/valorant-agent-selection/assets/agents/${agent}/artwork.png`;
+	};
+
+	const agentCards = document.querySelectorAll("div.agent-card");
+	agentCards.forEach(element => {
+		element.addEventListener('click', onClick(element.dataset.agent));
+	});
 }
 
 function submit() {
