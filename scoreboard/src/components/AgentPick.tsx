ac3dfde1b80d43aab9a522bbcebad8bfe0aa9994
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-01 20:09:03 +0800

    feat(scoreboard): add AgentPick component

---
+++ scoreboard/src/components/AgentPick.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import classNames from "classnames";
+
+import { Team } from "@/server/teams";
+
+import agents from "@/data/agents.json";
+
+export function AgentPick({
+  agent,
+  layout = "ltr",
+  team,
+}: {
+  layout?: "ltr" | "rtl";
+  team: Team;
+  agent: string | null;
+}) {
+  return (
+    <div
+      className={classNames(
+        "flex gap-3",
+        layout === "ltr" ? "flex-row" : "flex-row-reverse"
+      )}
+    >
+      <div className="border-[0.5px] p-[2px] border-white">
+        <div className="w-[5em] h-[5em] bg-white/30">
+          {agent && (
+            <Image
+              width="100%"
+              height="100%"
+              layout="responsive"
+              objectFit="contain"
+              src={`/assets/agents/${agent}/icon.png`}
+              alt={agent}
+            />
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col justify-center text-white">
+        <h2 className="text-xl font-bold">{team.name}</h2>
+        <span className={classNames("text-sm", !agent && "tracking-wider")}>
+          {agent ? agents[agent as keyof typeof agents].name : "Picking..."}
+        </span>
+      </div>
+    </div>
+  );
+}
