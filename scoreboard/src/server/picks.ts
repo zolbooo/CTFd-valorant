57bde1737c01999bf516cd9a5535c6be59432e7a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-01 19:50:37 +0800

    feat(scoreboard): add fetchAgentPicks function

---
+++ scoreboard/src/server/picks.ts
@@ -0,0 +1,23 @@
+import { APIResponse } from "./types";
+
+export type AgentChoice = {
+  id: number;
+  agent_name: string;
+  team_id: number;
+  team_name: string;
+};
+
+export async function fetchAgentPicks(): Promise<Record<string, string>> {
+  const result: APIResponse<AgentChoice[]> = await fetch(
+    `${process.env.CTFD_API_URL}/valorant/picks`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.CTFD_API_KEY}`,
+      },
+    }
+  ).then((res) => res.json());
+  return Object.fromEntries(
+    result.data.map(({ team_name, agent_name }) => [team_name, agent_name])
+  );
+}
