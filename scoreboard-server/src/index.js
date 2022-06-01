653c8bf6e850449fa0f578e246c8042ea4d1c41d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-01 18:43:07 +0800

    feat(scoreboard-server): listen on port from env variables

--- scoreboard-server/src/index.js
+++ scoreboard-server/src/index.js
@@ -1,14 +1,15 @@
-import { app, httpServer } from './app.js';
+import { app, httpServer } from "./app.js";
 
 import { webhookHandler } from "./webhook.js";
 
 app.get("/api/webhook", webhookHandler);
 
-httpServer.listen(3000, () => {
+const port = Number(process.env.PORT) || 3000;
+httpServer.listen(port, () => {
   const address = httpServer.address();
   console.log(
     `🌐 Listening on ${
       typeof address === "string" ? address : address.address
-    }:3000`
+    }:${port}`
   );
 });
