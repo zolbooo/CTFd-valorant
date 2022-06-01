75cf2e9bf105d536347b2161332bb2c228bdf589
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-01 13:53:50 +0800

    feat(select-agent): notify if agent was selected

--- CTFd/plugins/valorant-agent-selection/assets/js/select-agent.js
+++ CTFd/plugins/valorant-agent-selection/assets/js/select-agent.js
@@ -1,4 +1,19 @@
-let selectedAgent = null;
+let __selectedAgent = null;
+function setSelectedAgent(agent) {
+	__selectedAgent = agent;
+	if (agent !== null) {
+		submitButton.disabled = false;
+		artwork.hidden = false;
+		artwork.children[0].srcset = `/plugins/valorant-agent-selection/assets/agents/${agent}/artwork.webp`;
+		artwork.children[1].src = `/plugins/valorant-agent-selection/assets/agents/${agent}/artwork.png`;
+	} else {
+		submitButton.disabled = true;
+		artwork.hidden = true;
+	}
+}
+function getSelectedAgent() {
+	return __selectedAgent;
+}
 
 const artwork = document.getElementById('artwork');
 const submitButton = document.getElementById('submit');
@@ -7,28 +22,43 @@ function hydrate() {
 		if (e.target !== e.currentTarget) {
 			return;
 		}
-		selectedAgent = null;
-		submitButton.disabled = true;
-		artwork.hidden = true;
+		setSelectedAgent(null);
 	});
-	const onClick = agent => () => {
-		selectedAgent = agent;
-		submitButton.disabled = false;
-		artwork.hidden = false;
-		artwork.children[0].srcset = `/plugins/valorant-agent-selection/assets/agents/${agent}/artwork.webp`;
-		artwork.children[1].src = `/plugins/valorant-agent-selection/assets/agents/${agent}/artwork.png`;
-	};
 
 	const agentCards = document.querySelectorAll("div.agent-card");
 	agentCards.forEach(element => {
-		element.addEventListener('click', onClick(element.dataset.agent));
+		element.addEventListener('click', () => {
+			if (element.dataset.disabled) {
+				return;
+			}
+			setSelectedAgent(element.dataset.agent);
+		});
 	});
 }
 
+const eventSource = new EventSource('/events');
+eventSource.addEventListener('agent-selected', (message) => {
+	const {agent} = JSON.parse(message.data);
+
+	const agentCardOverlay = document.querySelector(`div.agent-card[data-agent="${agent}"] > .agent-overlay`);
+	agentCardOverlay.classList.add('agent-disabled');
+	agentCardOverlay.classList.remove('agent-overlay');
+
+	const agentCard = document.querySelector(`div.agent-card[data-agent="${agent}"]`);
+	agentCard.classList.remove('cursor-pointer');
+	agentCard.dataset.disabled = 'true';
+
+	if (getSelectedAgent() === agent) {
+		setSelectedAgent(null);
+	}
+});
 function submit() {
 	const input = document.querySelector('input[name="agent"]');
-	input.value = selectedAgent;
-	document.forms[0].submit();
+	input.value = getSelectedAgent();
+	if (input.value !== null) {
+		eventSource.close();
+		document.forms[0].submit();
+	}
 }
 
 hydrate();
