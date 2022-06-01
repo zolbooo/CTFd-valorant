e918ba09bc2d3702243f35b37fb9fe12ea8dec0c
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-01 21:18:14 +0800

    feat(scoreboard): add PickStage widget

--- scoreboard/src/pages/index.tsx
+++ scoreboard/src/pages/index.tsx
@@ -1,5 +1,29 @@
-function HomePage() {
-	return null;
+import { GetServerSideProps } from "next";
+
+import "@/core/SoundDispatcher";
+
+import { fetchTeams } from "@/server/teams";
+import { fetchAgentPicks } from "@/server/picks";
+
+import { PickStageInitialData, PickStageWidget } from "@/widgets/PickStage";
+
+export const getServerSideProps: GetServerSideProps<
+  PickStageInitialData
+> = async () => {
+  const [teams, agentPicks] = await Promise.all([
+    fetchTeams().then((res) => Array(10).fill(res).flat(1)),
+    fetchAgentPicks(),
+  ]);
+  return {
+    props: {
+      teams,
+      agentPicks,
+    },
+  };
+};
+
+function HomePage(initialData: PickStageInitialData) {
+  return <PickStageWidget initialData={initialData} />;
 }
 
 export default HomePage;
