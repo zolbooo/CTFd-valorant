0797b92f3d9391612515fd8d1f1de660249981af
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-03 04:44:10 +0800

    feat(scoreboard-server): add fetchCTFStatus function

---
+++ scoreboard/src/server/status.ts
@@ -0,0 +1,13 @@
+import { APIResponse } from "./types";
+
+export type CTFStatus = {
+  started: boolean;
+  ended: boolean;
+};
+
+export async function fetchCTFStatus() {
+  const result: APIResponse<CTFStatus> = await fetch(
+    `${process.env.CTFD_API_URL}/valorant/ctf-status`
+  ).then((res) => res.json());
+  return result.data;
+}
