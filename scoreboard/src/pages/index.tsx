70680b38e607293856a7bd9c2aa9e4ece86b2c10
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-03 05:21:58 +0800

    feat(scoreboard-widget): render list of teams from server

--- scoreboard/src/pages/index.tsx
+++ scoreboard/src/pages/index.tsx
@@ -3,27 +3,61 @@ import { GetServerSideProps } from "next";
 import "@/core/SoundDispatcher";
 
 import { fetchTeams } from "@/server/teams";
+import { fetchCTFStatus } from "@/server/status";
 import { fetchAgentPicks } from "@/server/picks";
+import { fetchScoreboard } from "@/server/scoreboard";
 
 import { PickStageInitialData, PickStageWidget } from "@/widgets/PickStage";
+import { ScoreboardInitialData, ScoreboardWidget } from "@/widgets/Scoreboard";
+
+type HomePageInitialData =
+  | {
+      started: false;
+      initialData: PickStageInitialData;
+    }
+  | {
+      started: true;
+      initialData: ScoreboardInitialData;
+    };
 
 export const getServerSideProps: GetServerSideProps<
-  PickStageInitialData
+  HomePageInitialData
 > = async () => {
-  const [teams, agentPicks] = await Promise.all([
-    fetchTeams().then((res) => Array(10).fill(res).flat(1)),
+  const [{ started }, teams, agentPicks] = await Promise.all([
+    fetchCTFStatus(),
+    fetchTeams(),
     fetchAgentPicks(),
   ]);
+  if (!started) {
+    return {
+      props: {
+        started: false,
+        initialData: {
+          teams,
+          agentPicks,
+        },
+      },
+    };
+  }
+
+  const scoreboard = await fetchScoreboard();
   return {
     props: {
-      teams,
-      agentPicks,
+      started: true,
+      initialData: {
+        teams,
+        scoreboard,
+        agentPicks,
+      },
     },
   };
 };
 
-function HomePage(initialData: PickStageInitialData) {
-  return <PickStageWidget initialData={initialData} />;
+function HomePage({ started, initialData }: HomePageInitialData) {
+  if (!started) {
+    return <PickStageWidget initialData={initialData} />;
+  }
+  return <ScoreboardWidget initialData={initialData} />;
 }
 
 export default HomePage;
--- scoreboard/src/widgets/Scoreboard.tsx
+++ scoreboard/src/widgets/Scoreboard.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
+import classNames from "classnames";
 
-export function ScoreboardWidget() {
+import { Team } from "@/server/teams";
+import { ScoreboardItem } from "@/server/scoreboard";
+
+export type ScoreboardInitialData = {
+  teams: Team[];
+  agentPicks: Record<string, string>;
+  scoreboard: ScoreboardItem[];
+};
+
+export function ScoreboardWidget({
+  initialData: { teams },
+}: {
+  initialData: ScoreboardInitialData;
+}) {
   return (
     <div className="w-screen h-screen bg-split bg-cover backdrop-blur-xl">
       <div className="w-full h-full bg-white/5 backdrop-blur-sm">
@@ -20,191 +34,32 @@ export function ScoreboardWidget() {
                 <p>DEATHS</p>
               </div>
             </div>
-            <div className="w-full h-16 mt-[2px] bg-teal-600 bg-opacity-60 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className="w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
+            {teams.map((team, index) => (
+              <div
+                key={team.name}
+                className={classNames(
+                  "w-full h-16 mt-[2px] bg-opacity-60 grid grid-flow-row-dense grid-cols-5 items-center",
+                  index % 2 === 0 ? "bg-teal-600" : "bg-rose-700"
+                )}
+              >
+                <div className="h-full flex flex-row items-center col-span-2">
+                  <div className="w-16">
+                    <Image
+                      width="100%"
+                      height="100%"
+                      layout="responsive"
+                      objectFit="contain"
+                      className="opacity-100"
+                      src="/assets/agents/astra/icon.png"
+                    />
+                  </div>
+                  <p className="px-8 text-white">ASTRA</p>
                 </div>
-                <p className="px-8 text-white">ASTRA</p>
+                <p className="text-white opacity-100 text-center">234</p>
+                <p className="text-white opacity-100 text-center">00</p>
+                <p className="text-white opacity-100 text-center">00</p>
               </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-
-            <div className="w-full h-16 mt-[2px] bg-rose-700 bg-opacity-50 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-            <div className="w-full h-16 mt-[2px] bg-teal-600 bg-opacity-60 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-
-            <div className="w-full h-16 mt-[2px] bg-rose-700 bg-opacity-50 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-            <div className="w-full h-16 mt-[2px] bg-teal-600 bg-opacity-60 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-
-            <div className="w-full h-16 mt-[2px] bg-rose-700 bg-opacity-50 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-            <div className="w-full h-16 mt-[2px] bg-teal-600 bg-opacity-60 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-
-            <div className="w-full h-16 mt-[2px] bg-rose-700 bg-opacity-50 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-            <div className="w-full h-16 mt-[2px] bg-teal-600 bg-opacity-60 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className=" w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
-
-            <div className="w-full h-16 mt-[2px] bg-rose-700 bg-opacity-50 grid grid-flow-row-dense grid-cols-5 items-center">
-              <div className="h-full flex flex-row items-center col-span-2">
-                <div className="w-16">
-                  <Image
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                    className="opacity-100"
-                    src="/assets/agents/astra/icon.png"
-                  />
-                </div>
-                <p className="px-8 text-white">ASTRA</p>
-              </div>
-              <p className="text-white opacity-100 text-center">234</p>
-              <p className="text-white opacity-100 text-center">00</p>
-              <p className="text-white opacity-100 text-center">00</p>
-            </div>
+            ))}
           </div>
         </div>
       </div>
