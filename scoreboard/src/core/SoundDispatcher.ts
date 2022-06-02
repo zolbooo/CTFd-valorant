1185f1c37ca3c917b3ffd9b19cfcb52c6539fb32
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-03 05:33:52 +0800

    refactor: remove unused SoundDispatcher class

--- scoreboard/src/core/SoundDispatcher.ts
+++ scoreboard/src/core/SoundDispatcher.ts
@@ -1,14 +1,3 @@
-import { AsyncTaskQueue } from "./AsyncTaskQueue";
-
-export class SoundDispatcher {
-  private asyncTaskQueue = new AsyncTaskQueue();
-
-  playSound(id: string): Promise<void> {
-    const audio = new Audio(`/assets/sounds/${id}.mp3`);
-    return this.asyncTaskQueue.push(() => audio.play());
-  }
-}
-
 export function playSound(id: string) {
   const audio = new Audio(`/assets/sounds/${id}.mp3`);
   return audio.play();
