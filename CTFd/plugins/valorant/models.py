9417feed79a463750f4b993676d8c3d4cba09ee3
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-04 16:46:17 +0800

    fix(agent-choice-model): add missing String length parameter for mysql dialect

--- CTFd/plugins/valorant/models.py
+++ CTFd/plugins/valorant/models.py
@@ -2,8 +2,8 @@ from CTFd.models import db
 
 class AgentChoice(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	agent_name = db.Column(db.String, unique=True)
+	agent_name = db.Column(db.String(32), unique=True)
 	team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
-	team_name = db.Column(db.String, unique=True)
+	team_name = db.Column(db.String(128), unique=True)
 	def __repr__(self) -> str:
 		return f'<AgentChoice {self.agent_name} {self.team_name}>'
