04d411e77caa454df80ce6d1d7e8660c80c25316
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-06-04 19:06:02 +0800

    feat: refresh page when CTF starts

---
+++ scoreboard/src/hooks/useCTFTime.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export function useCTFTime({ startAt }: { startAt: number }) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!startAt) {
+      return;
+    }
+
+    const now = Date.now() / 1000;
+    const timeUntilStart = startAt - now;
+    if (timeUntilStart > 0) {
+      const timeout = setTimeout(() => {
+        router.reload();
+      }, timeUntilStart);
+      return () => clearTimeout(timeout);
+    }
+  }, [router, startAt]);
+}
