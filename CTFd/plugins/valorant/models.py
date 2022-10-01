b7007a704831b07787ea6580b278e65536109c7f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-10-01 04:23:54 +0300

    fix(valorant): remove unique key constraint on agent_name column

--- CTFd/plugins/valorant/models.py
+++ CTFd/plugins/valorant/models.py
@@ -2,7 +2,7 @@ from CTFd.models import db
 
 class AgentChoice(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	agent_name = db.Column(db.String(32), unique=True)
+	agent_name = db.Column(db.String(32))
 	team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
 	team_name = db.Column(db.String(128), unique=True)
 	def __repr__(self) -> str:
